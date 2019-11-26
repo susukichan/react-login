de54e0bc7078868583447d59b949856790e1933f
HerolDinka <45111114+HerolDinka@users.noreply.github.com>
2019-11-26 22:14:13 +0100

    Added tabs to seperate login and register pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { UserForm } from "./components/UserForm";
+import { LoginTabs } from "./components/LoginTabs"
 
 function App() {
   return (
     <div className="App">
       <UserForm />
+      <LoginTabs />
     </div>
   );
 }
